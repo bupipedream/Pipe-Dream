67b781c16bf161a18e9c9b9495f6a72828a39783
daniel@danoc.me <daniel@danoc.me>
2012-04-22 17:56:12 -0400

    added new image sizes

--- functions/image-sizes.php
+++ functions/image-sizes.php
@@ -3,3 +3,9 @@
 // Register custom image sizes
 add_image_size('single-inline', '425', '425', null);
 add_image_size('alt-thumbnail', '125', '80', true);
+
+// Custom image sizes for homepage
+add_image_size('custom-431', '431');
+add_image_size('custom-165', '165');
+add_image_size('custom-222', '222');
+add_image_size('custom-75x75-crop', '75', '75', true);
