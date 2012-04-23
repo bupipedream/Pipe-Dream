8c7f69e44b228218c88006e083ab969a6612c393
daniel@danoc.me <daniel@danoc.me>
2012-04-22 20:34:42 -0400

    changed image sizes to correct dimensions

--- functions/image-sizes.php
+++ functions/image-sizes.php
@@ -5,7 +5,7 @@ add_image_size('single-inline', '425', '425', null);
 add_image_size('alt-thumbnail', '125', '80', true);
 
 // Custom image sizes for homepage
-add_image_size('custom-431', '431');
+add_image_size('custom-495', '495');
 add_image_size('custom-165', '165');
-add_image_size('custom-222', '222');
+add_image_size('custom-260', '260');
 add_image_size('custom-75x75-crop', '75', '75', true);
