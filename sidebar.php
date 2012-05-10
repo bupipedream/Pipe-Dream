001532dea21c5d045bb8cb1eafb73485d2ae612e
daniel@danoc.me <daniel@danoc.me>
2012-05-10 11:29:07 -0400

    removed ad spots and fancybox

--- sidebar.php
+++ sidebar.php
@@ -33,19 +33,19 @@
 		<!--/* OpenX Javascript Tag v2.8.8 */-->
 
 		<script type='text/javascript'><!--//<![CDATA[
-		   var m3_u = (location.protocol=='https:'?'https://www.bupipedream.com/openx/www/delivery/ajs.php':'http://www.bupipedream.com/openx/www/delivery/ajs.php');
-		   var m3_r = Math.floor(Math.random()*99999999999);
-		   if (!document.MAX_used) document.MAX_used = ',';
-		   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-		   document.write ("?zoneid=1&amp;block=1");
-		   document.write ('&amp;cb=' + m3_r);
-		   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-		   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-		   document.write ("&amp;loc=" + escape(window.location));
-		   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-		   if (document.context) document.write ("&context=" + escape(document.context));
-		   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-		   document.write ("'><\/scr"+"ipt>");
+		   // var m3_u = (location.protocol=='https:'?'https://www.bupipedream.com/openx/www/delivery/ajs.php':'http://www.bupipedream.com/openx/www/delivery/ajs.php');
+		   // var m3_r = Math.floor(Math.random()*99999999999);
+		   // if (!document.MAX_used) document.MAX_used = ',';
+		   // document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+		   // document.write ("?zoneid=1&amp;block=1");
+		   // document.write ('&amp;cb=' + m3_r);
+		   // if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+		   // document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+		   // document.write ("&amp;loc=" + escape(window.location));
+		   // if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+		   // if (document.context) document.write ("&context=" + escape(document.context));
+		   // if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+		   // document.write ("'><\/scr"+"ipt>");
 		//]]>--></script><noscript><a href='http://www.bupipedream.com/openx/www/delivery/ck.php?n=ad9e85cc&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://www.bupipedream.com/openx/www/delivery/avw.php?zoneid=1&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=ad9e85cc' border='0' alt='' /></a></noscript>
 
 	<!-- </section> -->
@@ -64,19 +64,19 @@
 			<!--/* OpenX Javascript Tag v2.8.8 */-->
 
 			<script type='text/javascript'><!--//<![CDATA[
-			   var m3_u = (location.protocol=='https:'?'https://www.bupipedream.com/openx/www/delivery/ajs.php':'http://www.bupipedream.com/openx/www/delivery/ajs.php');
-			   var m3_r = Math.floor(Math.random()*99999999999);
-			   if (!document.MAX_used) document.MAX_used = ',';
-			   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
-			   document.write ("?zoneid=2&amp;block=1");
-			   document.write ('&amp;cb=' + m3_r);
-			   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
-			   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
-			   document.write ("&amp;loc=" + escape(window.location));
-			   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
-			   if (document.context) document.write ("&context=" + escape(document.context));
-			   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
-			   document.write ("'><\/scr"+"ipt>");
+			   // var m3_u = (location.protocol=='https:'?'https://www.bupipedream.com/openx/www/delivery/ajs.php':'http://www.bupipedream.com/openx/www/delivery/ajs.php');
+			   // var m3_r = Math.floor(Math.random()*99999999999);
+			   // if (!document.MAX_used) document.MAX_used = ',';
+			   // document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+			   // document.write ("?zoneid=2&amp;block=1");
+			   // document.write ('&amp;cb=' + m3_r);
+			   // if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+			   // document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+			   // document.write ("&amp;loc=" + escape(window.location));
+			   // if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+			   // if (document.context) document.write ("&context=" + escape(document.context));
+			   // if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+			   // document.write ("'><\/scr"+"ipt>");
 			//]]>--></script><noscript><a href='http://www.bupipedream.com/openx/www/delivery/ck.php?n=a0bcea6a&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://www.bupipedream.com/openx/www/delivery/avw.php?zoneid=2&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=a0bcea6a' border='0' alt='' /></a></noscript>
 		<!-- </section> -->
 
