8271c0854f8b68d56f2ab845b7e63a1ff7fed615
Willie Demaniow <willie@SUPER-VIRUS-DOWNLOADEXE.local>
2012-10-10 12:35:01 -0400

    fixed facebook social bar word wrap

--- single-post.php
+++ single-post.php
@@ -174,7 +174,7 @@
 					<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 				</div>
 				
-				<div class="fb-like" data-href="<?php echo get_permalink(); ?>" data-send="true" data-width="450" data-show-faces="true"></div>
+				<div class="fb-like" data-href="<?php echo get_permalink(); ?>" data-send="true" data-width="500" data-show-faces="true"></div>
 			</section>
 			
 			<section id="comments">
