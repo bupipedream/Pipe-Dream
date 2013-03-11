7e5fe47426ad139bc97d95d47fb910d94e7e2986
Daniel O'Connor <daniel@danoc.me>
2013-03-10 20:05:58 -0400

    style improvements to search

--- search.php
+++ search.php
@@ -1,63 +1,54 @@
 <?php get_header(); ?>
 	<div class="row" id="content">
 		<div class="span17">
+			<?php global $wp_query; $posts_count = $wp_query->found_posts; ?>
+			<h1 class="page-title pad-left">
+				Search results for: <span class="highlight"><?= esc_attr( get_search_query() ); ?></span> <small>(<?= $posts_count.' result'.($posts_count === 1 ? '' : 's').' found' ?>)</small>
+			</h1>
 			
-			<section id="results">
-				
-				<!-- count the number of posts -->
-				<?php global $wp_query; $posts_count = $wp_query->found_posts; ?>
-				
-				<h2>Search Results for: <span class="highlight"><?php echo esc_attr(get_search_query()); ?></span><small>(<?php echo $posts_count.' result'.($posts_count == 1 ? '' : 's').' found' ?>)</small></h2>
-								
-				<?php 
-					$is_author = pd_is_author(get_search_query()); 
-					if(isset($is_author)):
-				?>
-				
-				<div class="alert">
-					<p>Are you searching for articles by <a href="<?php echo get_author_posts_url($is_author->ID); ?>"><?php echo $is_author->display_name; ?></a>?</p>
-				</div>
-				
-				<?php endif; ?>
-				
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-					
-					<!-- Check if the article is part of the archives -->
-					<?php $archive = pd_is_archived(get_the_ID()); ?>
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<section class="archive-list pad-left">
 
-					<?php // highlight the search results 
-						$title = get_the_title();
-						$excerpt = get_the_excerpt();
-	
-						$keys= explode(" ",$s); 
-							
-						$title = preg_replace('/('.implode('|', $keys) .')/iu', '<span class="highlight">\0</span>', $title); 
-						$excerpt = preg_replace('/('.implode('|', $keys) .')/iu', '<span class="highlight">\0</span>', $excerpt); 
+				<article class="clearfix">
+					<?php
+						$photos = get_photos( get_the_ID(), 1, array( 'alt-thumbnail' ));
+						if($photos):
 					?>
 					
-					<article class="clearfix">
-						
-						<!-- Grab all of the photos associated with article. -->
-						<?php 
-							$photos = get_photos(get_the_ID(), 1);
-							//if(!$photos) $photos = $archive['_image1'];
-						?>
-						
-						<?php if($photos): ?>
-							<figure>
-								<a href="<?php the_permalink() ?>">
-									<img src="<?php echo $photos['src']['thumbnail']; ?>" />
-								</a>
-							</figure>
-						<?php endif; ?>
+						<figure class="figure-right figure-border">
+							<a href="<?php the_permalink() ?>">
+								<img src="<?= ($photos['src']['alt-thumbnail']) ? $photos['src']['alt-thumbnail'] : $photos['src']['single-inline']; ?>" />
+							</a>
+						</figure>
 
-						<h3><a href="<?php the_permalink() ?>"><?php echo $title; ?></a></h3>
-						<p><?php echo $excerpt; ?></p>
-						<time datetime="<?php the_time('Y-m-j\Th:i'); ?>" title="<?php the_time('F j, Y \a\t g:i A T'); ?>"><?php the_time('F j, Y'); ?></time>
-					</article>
+					<? endif; ?>
 
-				<?php endwhile; ?>
-	
+					<h2 class="headline">
+						<a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+					</h2>
+
+					<div class="meta">By 
+						<?php if( function_exists( 'coauthors_posts_links' ) ): ?>
+							<?php if( is_coauthor_for_post('Staff Reports' ) ): ?>
+								<span class="author">Staff Reports</span>
+							<?php elseif( is_coauthor_for_post( 'archives' ) ): ?>
+								<span class="author"><?= pd_is_archived( get_the_ID(), '_author' ) ?></span>
+							<?php else: ?>
+								<span class="author"><?php coauthors(); ?></span>
+							<?php endif;?>
+						<?php endif;?>
+						- 
+						<time datetime="<?php the_time('Y-m-j\TH:i:sT'); ?>" title="<?php the_time('F j, Y \a\t g:i A T'); ?>"><?php the_time('F j, Y'); ?></time>
+					</div>
+					
+					<p class="excerpt"><?= get_the_excerpt(); ?></p>
+
+				</article>
+			
+			</section>
+
+			<?php endwhile; ?> 
+
 				<div id="pagination">
 					<?php		
 						global $wp_query;
@@ -66,21 +57,19 @@
 						echo paginate_links( array(
 							'base' => str_replace( $big, '%#%', get_pagenum_link( $big ) ),
 							'format' => '?paged=%#%',
-							'current' => max( 1, get_query_var('paged') ),
+							'current' => max( 1, get_query_var( 'paged' ) ),
 							'total' => $wp_query->max_num_pages
 						) );
 					?>
 				</div>
-				<?php else : ?>
-				
-				<article id="post-not-found">
-					<p>Sorry, your search for "<?php echo esc_attr(get_search_query()); ?>" did not match any articles.</p>
-				</article>
-				
-				<?php endif; ?>
 
-			</section>
+
+			<?php else: ?>
+				<p><?php _e('Sorry, no posts matched your criteria.'); ?></p>
+			<?php endif; ?>
+
 		</div>
 		<?php get_sidebar(); ?>
 	</div>
-	<?php get_footer(); ?>
+<?php get_footer(); ?>
+
