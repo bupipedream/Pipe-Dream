e9d3579c05f2fb777c63a7df1315518ee0a081c1
Daniel O'Connor <daniel@danoc.me>
2013-03-10 20:49:47 -0400

    added categories to footer

--- footer.php
+++ footer.php
@@ -1,43 +1,53 @@
-	<footer class="row" role="contentinfo">
-		<div id="footer-logo" class="span6">
-			<!-- Pipe Dream -->
-			<a href="<? bloginfo( 'wpurl' ); ?>" title="<?php bloginfo( 'name' ); ?>">
-				<img src="<? bloginfo( 'template_url' ); ?>/img/footerlogo.png" alt="<?php bloginfo( 'name' ); ?>" />
-			</a>
-		</div>
-
-		<nav id="footer-links" class="span12">
+</div>
+	<footer id="footer" class="row">
+		<nav id="footer-navigation">
 			<ul>
-				<li><a href="<?php bloginfo('wpurl'); ?>/news/">News</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/sports/">Sports</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/opinion/">Opinion</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/release/">Release</a></li>
-			</ul>
-			<ul>
-				<li>Established 1946</li>
-				<!-- <li><a href="<?php bloginfo('wpurl'); ?>/about/">About</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/contact/">Contact</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/contribute/">Contribute</a></li>
-				<li><a href="<?php bloginfo('wpurl'); ?>/staff/">Staff</a></li> -->
+				<li><span class="footer-category">News</span>
+					<ul class="footer-subcategories">
+						<li><a href="">Administration</a></li>
+						<li><a href="">Student Groups</a></li>
+						<li><a href="">Student Association</a></li>
+						<li><a href="">Police Watch</a></li>
+					</ul>
+				</li>
+				<li><span class="footer-category">Sports</span>
+					<ul class="footer-subcategories">
+						<li><a href="">Basketball</a></li>
+						<li><a href="">Basketball</a></li>
+						<li><a href="">Soccer</a></li>
+						<li><a href="">Baseball</a></li>
+						<li><a href="">Wrestling</a></li>
+						<li><a href="">Lacrosse</a></li>
+					</ul>
+				</li>
+				<li><span class="footer-category">Opinion</span>
+					<ul class="footer-subcategories">
+						<li><a href="">Columns</a></li>
+						<li><a href="">Editorial</a></li>
+						<li><a href="">Letters to the editor</a></li>
+						<li><a href="">Letters from the editor</a></li>
+					</ul>
+				</li>
+				<li><span class="footer-category">Release</span>
+					<ul class="footer-subcategories">
+						<li><a href="">Food</a></li>
+						<li><a href="">Movies</a></li>
+						<li><a href="">Music</a></li>
+						<li><a href="">Theatre</a></li>
+					</ul>
+				</li>
+				<li><span class="footer-category">Tools</span>
+					<ul class="footer-subcategories">
+						<li><a href="">About</a></li>
+						<li><a href="">Advertise</a></li>
+						<li><a href="">Contact</a></li>
+						<li><a href="">RSS</a></li>
+					</ul>
+				</li>
 			</ul>
 		</nav>
-		<div id="stabalizing" class="span4">
-			<!-- with love -->
-			<?php
-				$options = get_option( 'pd_theme_options' );
-				$stabilizing = $options['stabilizing'];
-				$destabilizing = $options['destabilizing'];
-			?>
-			<p>#stabilizing: <?= $stabilizing; ?><br />#destabilizing: <?= $destabilizing; ?></p>
-		</div>
-		<div id="wordpress" class="span2 last">
-			<!-- Powered by WordPress -->
-			<a href="http://wordpress.org/">
-				<img src="<?php bloginfo( 'template_url' ); ?>/img/wordpress.png" alt="WordPress" title="Proudly Powered by WordPress" />
-			</a>
-		</div>
+
 	</footer>
-</div>
 
 	<!-- JavaScript at the bottom for fast page loading -->
 
