3a448298eade8ca05cdd23fb8c3a031c17ccc582
Daniel O'Connor <daniel@danoc.me>
2013-03-10 20:59:26 -0400

    simplified default page template

--- page.php
+++ page.php
@@ -3,40 +3,20 @@
 		<div class="span17">
 			
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-						
-			<article id="post-<?php the_ID(); ?>">
-				
-				<!-- <p class="published">
-					<time datetime="<?php the_time('Y-m-j\TH:i:sT'); ?>" title="Published on <?php the_time('F j, Y \a\t g:i A T'); ?>">
-						<?php the_time('F j, Y'); ?>
-					</time>
-				</p> -->
-				
-				<h2><?php the_title(); ?></h2>
-				
-				<!-- <div id="meta">					
-				</div> -->
-				
-				<section> <!-- article text and images -->
-					<?php the_content(); ?>
-				</section>
-			</article>
-						
-			<?php endwhile; ?>
-						
-			<?php else : ?>
-
-			<article id="post-not-found">
-			    <header>
-			    	<h1>Not Found</h1>
-			    </header>
-			    <section class="post_content">
-			    	<p>Sorry, but the requested page was not found on this site.</p>
-			    </section>
-			    <footer>
-			    </footer>
-			</article>
 			
+				<article class="pad-left pad-right" id="post-<?php the_ID(); ?>">
+					
+					<h2 class="page-title"><?php the_title(); ?></h2>
+					
+					<section class="pad-left pad-right"> <!-- article text and images -->
+						<?php the_content(); ?>
+					</section>
+				</article>
+			
+			<?php endwhile; ?>
+			
+			<?php else : ?>
+				<p>Page not found.</p>			
 			<?php endif; ?>
 
 		</div>
