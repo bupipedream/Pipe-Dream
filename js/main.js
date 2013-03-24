3bc3c92ee77987f108e74bb83b72ea68b6c0834f
Daniel O'Connor <daniel@danoc.me>
2013-03-24 11:15:09 -0400

    show the correct weather icons

--- js/main.js
+++ js/main.js
@@ -23,8 +23,72 @@ $(document).ready(function() {
 		zipcode: '13902',
 		unit: 'f',
 		success: function(weather) {
+			var iconSrc = $("#weather").data('template-url') + '/img/weather/';
+			// Weather codes from Yahoo!
+			// http://developer.yahoo.com/weather/#codes
+			switch(parseInt(weather.code, 10)) {
+				case 1: // tornado
+				case 3: // hurricane
+				case 6: iconSrc += 'hurricane.png'; break; // mixed rain and sleet
+
+				case 2: // tropical storm
+				case 4: // severe thunderstorms
+				case 37: // isolated thunderstorms
+				case 38: // scattered thunderstorms
+				case 39: // scattered thunderstorms
+				case 45: // thundershowers
+				case 47: iconSrc += 'lightning.png'; break; // isolated thundershowers
+
+				case 5: // mixed rain and snow
+				case 7: // mixed snow and sleet
+				case 13: // snow flurries
+				case 14: // light snow showers
+				case 15: // blowing snow
+				case 16: // snow
+				case 17: // hail
+				case 18: // sleet
+				case 41: // heavy snow
+				case 43: // heavy snow
+				case 46: iconSrc += 'snow.png'; break; // snow showers
+
+				case 8: // freezing drizzle
+				case 9: // drizzle
+				case 10: // freezing rain
+				case 11: // showers
+				case 12: // showers
+				case 35: // mixed rain and hail
+				case 40: iconSrc += 'rain.png'; break; // scattered showers
+
+				case 19: // dust
+				case 25: // cold
+				case 26: // cloudy
+				case 27: // mostly cloudy (night)
+				case 28: // mostly cloudy (day)
+				case 29: // partly cloudy (night)
+				case 30: iconSrc += 'mostly-cloudy.png'; break; // partly cloudy (day)
+
+				case 20: // foggy
+				case 21: // haze
+				case 22: iconSrc += 'fog.png'; break; // smoky
+
+				case 23: // blustery
+				case 24: iconSrc += 'wind.png'; break; // windy
+
+				case 31: // clear (night)
+				case 32: // sunny
+				case 36: iconSrc += 'sun.png'; break; // hot
+
+				case 33: // fair (night)
+				case 34: // fair (day)
+				case 44: iconSrc += 'partly-sunny.png'; break; // partly cloudy
+
+				case 42: iconSrc += 'sun-shower.png'; break; // scattered snow showers
+
+				default: iconSrc = false; break;
+			}
+
 			html = '<span title="' + weather.currently + ' and ' + weather.temp + ' degrees in '+ weather.city +', '+weather.region+'">'
-			html += '<img src="' + $("#weather").data('template-url') + '/img/weather/partly-sunny.png" alt="Party Sunny">';
+			if(iconSrc) html += '<img src="' + iconSrc + '" alt="' + weather.currently + '">';
 			html += weather.temp + '&deg</span>';		
 			$("#weather").html(html);
 		},
