1d16e95a24351e45b8ca1e0cd5565e221568adc1
Daniel O'Connor <daniel@danoc.me>
2013-03-27 14:44:03 -0400

    redesigned the about page and added more pages to the navigation

---
+++ page-about.php
@@ -0,0 +1,141 @@
+<?php get_header(); ?>
+
+	<!-- Get all of content on the homepage -->
+	<?php $sections = get_sections(); ?>
+		
+	<div id="page-advertise" class="content row">
+		<nav id="page-navigation">
+			<ul>
+				<li class="active"><a href="<? bloginfo('wpurl'); ?>/about/" title="About Pipe Dream">About</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/advertise/" title="Advertise in Pipe Dream">Advertise</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/donate/" title="Donate to Pipe Dream">Donate</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/join/" title="Join Pipe Dream">Join</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/staff/" title="Faces behind Pipe Dream">Staff</a></li>
+				<li><a href="<? bloginfo('wpurl'); ?>/contact/" title="Contact Pipe Dream">Contact</a></li>
+			</ul>
+		</nav>
+		<h1 class="page-title">About Pipe Dream</h1>
+		<section class="row post">
+			<div class="span16">
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vel mauris odio, ut fringilla sapien. Donec porttitor scelerisque orci vitae ullamcorper. Nam vulputate dui sed urna egestas in pulvinar eros.</p>
+				
+				<h2>History</h2>
+				<p>Founded in 1946 as The Colonial News, Pipe Dream is Binghamton University’s oldest and largest student newspaper. Pipe Dream is published independently by an Executive Board composed entirely of undergraduate students, without the supervision or assistance of an adviser.</p>
+				<p>Pipe Dream publishes on Tuesdays and Fridays during the semester while the website is constantly updated with new content and breaking news.</p>
+				
+				<h2>Join Pipe Dream</h2>
+				<p>Aenean eget tortor ante, sed eleifend augue. Integer non libero eu arcu mattis faucibus et sed elit. Aliquam et mi vitae tortor mattis ultricies id vel enim. Suspendisse ligula mi, elementum in ullamcorper sit amet, ultrices sed felis.</p>
+				<p>Integer molestie rutrum nisl, mollis aliquam erat lobortis vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin orci quam, tincidunt sed sodales eget, pretium quis velit. Donec lacus erat, porttitor vitae tincidunt sit amet.</p>
+
+				<h2>Advertise</h2>
+				<p>Maliquam erat lobortis vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin orci quam, tincidunt sed sodales eget, pretium quis velit. Donec lacus erat, porttitor vitae tincidunt sit amet.</p>
+
+				<h2>Donations</h2>
+				<p>Maliquam erat lobortis vitae. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin orci quam, tincidunt sed sodales eget, pretium quis velit. Donec lacus erat, porttitor vitae tincidunt sit amet.</p>
+			</div>
+
+			<div class="sidebar span8 last">
+				<section id="masthead">
+					<!-- <h1>Pipe Dream Staff</h1> -->
+					<ul>
+						<li>
+							<span class="board">Editor-in-Chief</span>
+							<ul>
+								<li>Daniel Weintraub '13</li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Managing Editor</span>
+							<ul>
+								<li>Jules Forrest '14</li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Business</span>
+							<ul>
+								<li>Kimberly Bower '13, <em>Business Manager</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">News</span>
+							<ul>
+								<li>James Galloway '13, <em>News Editor</em></li>
+								<li>Geoff Wilson '14, <em>Assistant News Editor</em></li>
+								<li>Christina Pullano '14, <em>Assistant News Editor</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Sports</span>
+							<ul>
+								<li>Megan Brockett '13, <em>Sports Editor</em></li>
+								<li>Ari Kramer '14, <em>Assistant Sports Editor</em></li>
+								<li>Erik Bacharach '14, <em>Assistant Sports Editor</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Photo</span>
+							<ul>
+								<li>Jonathan Heisler '13, <em>Photo Editor</em></li>
+								<li>Kendall Loh '14, <em>Assistant Photo Editor</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Opinion</span>
+							<ul>
+								<li>Kaitlin Busser '13, <em>Opinion Editor</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Release</span>
+							<ul>
+								<li>Darian Lusk '14, <em>Release Editor</em></li>
+								<li>Jacob Shamsian '15 <em>Assistant Release Editor</em><li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Design</span>
+							<ul>
+								<li>Paige Nazinitsky '14, <em>Design Manager</em></li>
+								<li>Zack Feldman '15, <em>Design Assistant</em></li>
+								<li>Rebecca Forney '14, <em>Design Assistant</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Copy Desk</span>
+							<ul>
+								<li>Kaitlin Busser '13, <em>Copy Desk Chief</em></li>
+								<li>Tina Ritter '13, <em>Assistant Copy Desk Chief</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Fun Page</span>
+							<ul>
+								<li>Mike Manzi '13, <em>Fun Page Editor</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Editorial Artist</span>
+							<ul>
+								<li>Mariam Geiger '13, <em>Editorial Artist</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Web</span>
+							<ul>
+								<li>Daniel O'Connor '14, <em>Lead Web Developer</em></li>
+								<li>Willie Demaniow '16, <em>Asst. Web Developer</em></li>
+								<li>Melissa Edelblum '15, <em>Social Media Manager</em></li>
+							</ul>
+						</li>
+						<li>
+							<span class="board">Technology</span>
+							<ul>
+								<li>Derek Parry '13, <em>Newsroom Tech Manager</em></li>
+							</ul>
+						</li>
+					</ul>
+				</section>
+			</div>
+		</section>
+	</div>
+	<?php get_footer(); ?>
