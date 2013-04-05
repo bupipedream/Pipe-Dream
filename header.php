012352c67f897a1cf396577dfc43757f0bd9e06e
Daniel O'Connor <daniel@danoc.me>
2013-04-04 20:10:50 -0400

    use openx code for 728x90 ad instead

--- header.php
+++ header.php
@@ -352,15 +352,24 @@
 </script>
 
 <div id="ad-720x90">
-	<!-- begin ad tag -->
-	<script type="text/javascript">
-		//<![CDATA[
-		ord=Math.random()*10000000000000000;
-		document.write('<script type="text/javascript" src="http://ad.doubleclick.net/adj/sunybinghamton.afc/;kw=campbaruchaprmay2013ny016m720x90;sz=728x90;ord=' + ord + '?"><\/script>');
-		//]]>
-	</script>
-	<noscript><a href="http://ad.doubleclick.net/jump/sunybinghamton.afc/;kw=campbaruchaprmay2013ny016m720x90;sz=728x90;ord=123456789?" target="_blank" ><img src="http://ad.doubleclick.net/ad/sunybinghamton.afc/;kw=campbaruchaprmay2013ny016m720x90;sz=728x90;ord=123456789?" border="0" alt="" /></a></noscript>
-	<!-- end ad tag -->
+	<!--/* OpenX Javascript Tag v2.8.10 */-->
+
+	<script type='text/javascript'><!--//<![CDATA[
+	   var m3_u = (location.protocol=='https:'?'https://www.bupipedream.com/openx/www/delivery/ajs.php':'http://www.bupipedream.com/openx/www/delivery/ajs.php');
+	   var m3_r = Math.floor(Math.random()*99999999999);
+	   if (!document.MAX_used) document.MAX_used = ',';
+	   document.write ("<scr"+"ipt type='text/javascript' src='"+m3_u);
+	   document.write ("?zoneid=3");
+	   document.write ('&amp;cb=' + m3_r);
+	   if (document.MAX_used != ',') document.write ("&amp;exclude=" + document.MAX_used);
+	   document.write (document.charset ? '&amp;charset='+document.charset : (document.characterSet ? '&amp;charset='+document.characterSet : ''));
+	   document.write ("&amp;loc=" + escape(window.location));
+	   if (document.referrer) document.write ("&amp;referer=" + escape(document.referrer));
+	   if (document.context) document.write ("&context=" + escape(document.context));
+	   if (document.mmm_fo) document.write ("&amp;mmm_fo=1");
+	   document.write ("'><\/scr"+"ipt>");
+	//]]>--></script><noscript><a href='http://www.bupipedream.com/openx/www/delivery/ck.php?n=afb78f05&amp;cb=INSERT_RANDOM_NUMBER_HERE' target='_blank'><img src='http://www.bupipedream.com/openx/www/delivery/avw.php?zoneid=3&amp;cb=INSERT_RANDOM_NUMBER_HERE&amp;n=afb78f05' border='0' alt='' /></a></noscript>
+
 </div>
 
 <div id="container">
