680cbf3659d9de1753b7929c241e0322fc590d05
Daniel O'Connor <daniel@danoc.me>
2013-04-11 13:04:15 -0400

    update default page style and code

--- page.php
+++ page.php
@@ -1,25 +1,9 @@
 <?php get_header(); ?>
-	<div class="content row">
-		<div class="span17">
-			
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			
-				<article class="post pad-left pad-right" id="post-<?php the_ID(); ?>">
-					
-					<h1 class="page-title"><?php the_title(); ?></h1>
-					
-					<section class="pad-left pad-right"> <!-- article text and images -->
-						<?php the_content(); ?>
-					</section>
-				</article>
-			
-			<?php endwhile; ?>
-			
-			<?php else : ?>
-				<p>Page not found.</p>			
-			<?php endif; ?>
 
-		</div>
-		<?php get_sidebar(); ?>
+	<div class="content row">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+		<section class="post">
+			<?php the_content(); ?>
+		</section>
 	</div>
 	<?php get_footer(); ?>
