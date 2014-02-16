90cb8eb764f6330c4ac9c50b55dd86ce149f642d
Daniel O'Connor <daniel@danoc.me>
2014-02-16 16:45:11 -0500

    remove old/unused plugins

--- wp-content/plugins/cdn-sync-tool/lib/Cst/Page/Catn.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Cst_Page_Catn extends Cst_Page {
-	
-	public function display($test = false){
-		require CST_DIR.'/pages/misc/catn.html';
-	}
-	
-}
--- wp-content/plugins/cdn-sync-tool/lib/Cst/Page/Help.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Cst_Page_Help extends Cst_Page {
-	
-	public function display(){
-		require_once CST_DIR."/pages/misc/help.html";
-	}
-	
-}
